80441ab2a6f20d37e7b35ad0cf031afd7c02a284
jamesboot <james.boot.94@gmail.com>
2026-02-06 14:33:14 +0000

    Correct singularity bind and build remotely on sylabs.

--- singularity/r-analysis-build.sh
+++ singularity/r-analysis-build.sh
@@ -2,7 +2,14 @@
 
 # Bash script for building the R environment for the analysis of viral junctions/splicing/recombination in nf-vjunc
 # This script will be used in the singularity container to set up the R environment with the necessary packages for the analysis
+# Note access token for singularity hub is required to build the container
+# Mine is found: /camp/home/bootj/.singularity/remote.yaml
 
+# Load singularity module
 ml Singularity/3.11.3
 
-singularity build --fakeroot r-analysis.sif r-analysis.def
+# Disable bind paths for remote build
+export SINGULARITY_BIND=""
+
+# Build remotely
+singularity build --remote r-analysis.sif r-analysis.def
